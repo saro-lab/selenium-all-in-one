7702014adfa26fcc7ff28f964199859fed711d16
PARK Yong Seo <j@saro.me>
2024-06-16 22:08:39 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,140 @@
+import java.util.*
+
+/**
+ * + publish
+ * 1. gradle publish
+ * 2. https://oss.sonatype.org/
+ * 3. Staging Repositories
+ * 4. Close -> Release
+ *
+ * + publish setting
+ * 1. create gpg
+ * 2. set gradle.properties
+ *    - ex windows path) C:/Users/<USER_NAME>/.gradle/gradle.properties
+ *    sonatype.username=<username>
+ *    sonatype.password=<password>
+ *    signing.keyId=<last 8/16 chars in key>
+ *    signing.password=<secret>
+ *    signing.secretKeyRingFile=<path of secring.gpg>
+ *
+ * + you can use "User Token" instead of id & password.
+ *     - https://oss.sonatype.org -> profile -> User Token
+ *
+ * @See
+ * https://github.com/saro-lab/jwt
+ * https://docs.gradle.org/current/userguide/publishing_maven.html
+ * https://docs.gradle.org/current/userguide/signing_plugin.html#signing_plugin
+ * windows -> pgp4win
+ * gpg --gen-key
+ * gpg --list-keys --keyid-format short
+ * gpg --export-secret-keys -o secring.gpg
+ */
+
+plugins {
+    val kotlinVersion = "2.0.0"
+    id("org.jetbrains.kotlin.jvm") version kotlinVersion
+    id("org.jetbrains.kotlin.kapt") version kotlinVersion
+    signing
+    `maven-publish`
+}
+
+val properties = Properties()
+file("/src/main/resources/application.properties").inputStream().use { properties.load(it) }
+
+val seleniumVersion = properties["selenium.version"]
+val version = "$seleniumVersion.0"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+dependencies {
+    // selenium
+    api("org.seleniumhq.selenium:selenium-java:$seleniumVersion")
+
+    // test
+    testImplementation("org.junit.jupiter:junit-jupiter-api:+")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:+")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+
+            groupId = "me.saro"
+            artifactId = "selenium-chrome-all-in-one"
+            version = version
+
+            from(components["java"])
+
+            repositories {
+                maven {
+                    credentials {
+                        try {
+                            username = project.property("sonatype.username").toString()
+                            password = project.property("sonatype.password").toString()
+                        } catch (e: Exception) {
+                            println("warn: " + e.message)
+                        }
+                    }
+                    val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+                    val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+                    url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+                }
+            }
+
+            pom {
+                name.set("Selenium All-in-One")
+                description.set("Selenium All-in-One")
+                url.set("https://saro.me")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("PARK Yong Seo")
+                        email.set("j@saro.me")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/saro-lab/selenium-all-in-one.git")
+                    developerConnection.set("scm:git:git@github.com:saro-lab/selenium-all-in-one.git")
+                    url.set("https://github.com/saro-lab/selenium-all-in-one")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["maven"])
+}
+
+tasks.withType<Javadoc>().configureEach {
+    options {
+        this as StandardJavadocDocletOptions
+        addBooleanOption("Xdoclint:none", true)
+    }
+}
+
+configure<JavaPluginExtension> {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
